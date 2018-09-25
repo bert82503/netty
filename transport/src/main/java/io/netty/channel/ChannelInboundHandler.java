44104316e6a911bb34ad10a77f77c8abd1ad73a7
dannong <dannong@wacai.com>
2018-09-25 10:20:41 +0800

    Comment <ChannelInboundHandler的生命周期方法>

--- transport/src/main/java/io/netty/channel/ChannelInboundHandler.java
+++ transport/src/main/java/io/netty/channel/ChannelInboundHandler.java
@@ -26,34 +26,38 @@ public interface ChannelInboundHandler extends ChannelHandler {
     // Channel的生命周期
     /// Channel的生命周期状态
     /// Channel的状态模型(Registered -> Active -> Inactive -> Unregistered)
+
+    /// ChannelInboundHandler的生命周期方法
     /**
      * The {@link Channel} of the {@link ChannelHandlerContext} was registered with its {@link EventLoop}.
-     * Channel已经被注册到EventLoop。
+     * Channel已经注册到它的EventLoop。(能够处理I/O)
      */
     void channelRegistered(ChannelHandlerContext ctx) throws Exception;
 
     /**
      * The {@link Channel} of the {@link ChannelHandlerContext} was unregistered from its {@link EventLoop}.
-     * Channel从EventLoop取消注册。
+     * Channel从它的EventLoop注销。(无法处理任何I/O)
      */
     void channelUnregistered(ChannelHandlerContext ctx) throws Exception;
 
     /**
      * The {@link Channel} of the {@link ChannelHandlerContext} is now active.
-     * Channel现在处于活动状态(已经连接到它的远程节点)，可以接收和发送数据。
+     * Channel现在处于活动状态。(已经连接/绑定并且已经就绪)
+     * (已经连接到它的远程节点，可以接收和发送数据)
      */
     void channelActive(ChannelHandlerContext ctx) throws Exception;
 
     /**
      * The {@link Channel} of the {@link ChannelHandlerContext} was registered is now inactive and reached its
      * end of lifetime.
-     * Channel现在处于非活动状态(没有连接到远程节点)，且达到生命的终点。
+     * Channel离开活动状态，并且达到生命的终点。(不再连接它的远程节点)
+     * (没有连接到远程节点)
      */
     void channelInactive(ChannelHandlerContext ctx) throws Exception;
 
-
     /**
      * Invoked when the current {@link Channel} has read a message from the peer.
+     * 当前Channel从对等方读取消息时被调用。
      */
     void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception;
 
@@ -62,6 +66,8 @@ public interface ChannelInboundHandler extends ChannelHandler {
      * {@link #channelRead(ChannelHandlerContext, Object)}.  If {@link ChannelOption#AUTO_READ} is off, no further
      * attempt to read an inbound data from the current {@link Channel} will be made until
      * {@link ChannelHandlerContext#read()} is called.
+     * 当前读取操作读取到的最后一条消息被{@link #channelRead(ChannelHandlerContext, Object)}方法消费后被调用。
+     * (当Channel上的一个读操作完成时被调用)
      */
     void channelReadComplete(ChannelHandlerContext ctx) throws Exception;
 
@@ -73,6 +79,7 @@ public interface ChannelInboundHandler extends ChannelHandler {
     /**
      * Gets called once the writable state of a {@link Channel} changed. You can check the state with
      * {@link Channel#isWritable()}.
+     * 当Channel的可写状态发生改变时被调用。
      */
     void channelWritabilityChanged(ChannelHandlerContext ctx) throws Exception;
 
